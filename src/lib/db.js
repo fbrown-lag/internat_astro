7ddc75ac1b32204789652c1a2fee5cc41b5c35fd
nincapable <poirothugo38@gmail.com>
2026-05-28 11:30:19 +0200

    endpoint toujours

--- src/lib/db.js
+++ src/lib/db.js
@@ -7,9 +7,26 @@ export function getDatabaseUrl() {
     return process.env.POSTGRES_URL || process.env.DATABASE_URL;
 }
 
+function getSslConfig() {
+    if (process.env.POSTGRES_SSL === 'true') {
+        return true;
+    }
+
+    if (process.env.POSTGRES_SSL_REJECT_UNAUTHORIZED === 'false') {
+        return {
+            rejectUnauthorized: false,
+        };
+    }
+
+    return undefined;
+}
+
+const sslConfig = getSslConfig();
+
 // Use connectionString from environment variables for better security and flexibility
 export const pool = new Pool({
     connectionString: getDatabaseUrl(),
+    ...(sslConfig ? { ssl: sslConfig } : {}),
 });
 
 /**
